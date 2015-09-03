70037bc9dc80c2a93a63e131b6745c147d3f18b0
Fabian Viermetz <fabs.viermetz@live.com>
2015-09-03 18:56:46 +0200

    completed ppm output but not tested yet

--- pwm_ppm_converter/pwm_ppm_converter.ino
+++ pwm_ppm_converter/pwm_ppm_converter.ino
@@ -1,10 +1,16 @@
-
-#define CHANNELS 8
+#include "Arduino.h"
+#define CHANNELS 16
 
 #define MAX_PULSE 2100
 #define MIN_PULSE 900
 #define PULSE_PRESET 1500
-#define DEADBAND 20
+#define DEADBAND 10
+
+#define CHANNELS_W_ENDPULSE (CHANNELS+1)
+
+#define PPM_PULSE_LEN 300
+#define PPM_END_PULSE 5000
+#define PPM_POLARITY HIGH
 
 #if CHANNELS > 16
 #error : More than 16 Channels not suported!
@@ -12,42 +18,50 @@
 
 volatile uint32_t cur_micros = 0;
 volatile uint32_t cur_state, last_state, change;
-volatile uint32_t pcint_last_change[CHANNELS];
-volatile int16_t pwm[CHANNELS];
+volatile uint16_t pcint_last_change[CHANNELS];
+volatile int16_t pwm[CHANNELS_W_ENDPULSE], temp_pwm[CHANNELS];
 
 void setup() {
   // setting all pins as inputs except A3 as our PPM pin as output
   DDRD = B00000000; /*D0~D7*/
-  DDRB = B00000000; /*D8~D13*/
-  DDRC = B00001000; /*A0~A7*/
+  DDRC = B00000000; /*A0~A7*/
 
   /* following code is setting up the interrupts. essential for this project */
 
   // enable general Pin Change Interrupts
   PCICR |= (1 << PCIE2);             /*D0~D7*/
 #if CHANNELS > 8
-  PCICR |= (1 << PCIE0);             /*D8~D13*/
-#if CHANNELS > 14
-  PCICR |= (1 << PCIE1);             /*A0~A7*/
-#endif
+  PCICR |= (1 << PCIE0);             /*A0~A7*/
 #endif
 
-  // enable pins to trigger interrupts. up to D0~D13 + A0~A1 = 16 Channels
-  for (uint8_t i = 0 ; ((i < CHANNELS) && (i <  8)); i++) PCMSK2 |= (1 <<  i);             /*D0~D7*/
-  for (uint8_t i = 8 ; ((i < CHANNELS) && (i < 14)); i++) PCMSK0 |= (1 << (i - 8));        /*D8~D13*/
-  for (uint8_t i = 14 ; ((i < CHANNELS) && (i < 16)); i++) PCMSK1 |= (1 << (i - 14));      /*A0~A1*/
+  // enable pins to trigger interrupts. up to D0~D7 + A0~A7 = 16 Channels
+  for (uint8_t i = 0 ; (i < CHANNELS) && (i <  8); i++) PCMSK2 |= (1 <<  i);             /*D0~D7*/
+  for (uint8_t i = 8 ; i < CHANNELS; i++)               PCMSK0 |= (1 << (i - 8));        /*A0~A7*/
 
   for (uint8_t i = 0 ; i < CHANNELS; i++) pwm[i] = PULSE_PRESET;
+  pwm[CHANNELS_W_ENDPULSE] = PPM_END_PULSE;
+
+  // ppm out
+  pinMode(10, OUTPUT);
+  digitalWrite(10, PPM_POLARITY);
+  OCR1A = PPM_PULSE_LEN*2;                     // compare match register, change this
+  TCCR1B |= (1 << WGM12);          // turn on CTC mode
+  TCCR1B |= (1 << CS11);           // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A);         // enable timer compare interrupt
+
+  Serial.begin(115200);
+
 }
 
 void loop () {
   // nothing happens here since everything is interrupt trigered
+  Serial.println(pwm[2]);
+  delay(20);
 }
 
 void ISR_function() {
   static int16_t temp_pulse;
-  static uint8_t ready_to_kill[CHANNELS];
-  cur_state = (PIND | (PINB << 8) | (PINC << 14)); // reads current state of all pins
+  cur_state = (PIND | (PINC << 8)); // reads current state of all pins
   change = cur_state ^ last_state;               // compares with last values
   last_state = cur_state;                         // updating last values with the new ones
 
@@ -55,14 +69,7 @@ void ISR_function() {
     if ((change >> i) & 1UL == 1) {
       temp_pulse = (cur_micros - pcint_last_change[i]);
       if ((temp_pulse < MAX_PULSE) && (temp_pulse > MIN_PULSE)) {
-        if ((abs(temp_pulse - pwm[i]) > DEADBAND) && (ready_to_kill[i])) {
-          ready_to_kill[i] = 0;
-          continue;
-        }
-        else {
-          pwm[i] = (temp_pulse >> 1) << 1;
-          ready_to_kill[i] = 1;
-        }
+        temp_pwm[i] = temp_pulse;
       }
       else {
         pcint_last_change[i] = cur_micros;
@@ -71,12 +78,58 @@ void ISR_function() {
   }
 }
 
-#define COMPUTE_PCINT {cur_micros = micros(); sei(); ISR_function();}
+void calculate_pulses() {
+  static uint8_t ready_to_kill[CHANNELS];
+  for ( uint8_t i = 0; i < CHANNELS; i++) {
+    if ((abs(temp_pwm[i] - pwm[i]) > DEADBAND) && (ready_to_kill[i])) {
+      ready_to_kill[i] = 0;
+    }
+    else {
+      pwm[i] = (temp_pwm[i] >> 1) << 1;
+      ready_to_kill[i] = 1;
+    }
+  }
+}
 
-ISR(PCINT2_vect) COMPUTE_PCINT        // interrupt service routine for port B, D0~D7
+
+ISR(PCINT2_vect) {
+  cur_micros = micros();  // interrupt service routine for port B, D0~D7
+  sei();
+  ISR_function();
+}
 #if CHANNELS > 8
-ISR(PCINT1_vect) COMPUTE_PCINT        // interrupt service routine for port D, D8~D13
-#if CHANNELS > 14
-ISR(PCINT0_vect) COMPUTE_PCINT        // interrupt service routine for port C, A0~A1
+ISR(PCINT0_vect) {
+  cur_micros = micros();  // interrupt service routine for port C, A0~A7
+  sei();
+  ISR_function();
+}
 #endif
+
+
+#if PPM_POLARITY
+    #define TOGGLE_REST_PULSE  PINB &= ~(1 << 2)
+    #define TOGGLE_BEGIN_PULSE PINB |= (1 << 2)
+#else
+    #define TOGGLE_REST_PULSE  PINB |= (1 << 2)
+    #define TOGGLE_BEGIN_PULSE PINB &= ~(1 << 2)
 #endif
+
+ISR (TIMER1_COMPA_vect) {
+  static uint8_t i = 0;
+  TCNT1 = 0;
+  if (OCR1A < (PPM_PULSE_LEN << 1)+50) {
+    TOGGLE_REST_PULSE;
+    OCR1A = (pwm[(i++%CHANNELS_W_ENDPULSE)-1] - PPM_PULSE_LEN) << 1;
+    if (i == 0) {
+      sei();
+      calculate_pulses();
+    }
+  }
+  else {
+    TOGGLE_BEGIN_PULSE;
+    OCR1A = PPM_PULSE_LEN;
+  }
+}
+
+
+
